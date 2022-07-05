6187af957ddd93caf44e8c724e3a0e8f24019ff5
yuxunan <106828763+yuxunan@users.noreply.github.com>
2022-07-05 14:16:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -115,3 +115,41 @@ function checkScope() {
   console.log('Function scope i is: ', i);
   return i;
 }
+
+
+// Open console to view output
+function freezeObj() {
+  "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // change code below this line
+
+  Object.freeze(MATH_CONSTANTS);
+
+  // change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch( ex ) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+// Should show [object Error] {}
+
+const magic = () =>  new Date();
+
+const increment = (number, value = 1) => number + value;
+
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
+
+
+let a = 8, b = 6;
+// change code below this line
+[a, b] = [b, a];
+// change code above this line
+console.log(a); // should be 6
+console.log(b); // should be 8
