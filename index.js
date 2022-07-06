ab612b2aa9a163a080aac6e2962ed97df877ddad
yuxunan <106828763+yuxunan@users.noreply.github.com>
2022-07-06 11:49:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -338,3 +338,123 @@ console.log(checkInventory("apples"));
 
 
 
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+// Only change code below this line
+delete foods.oranges;
+delete foods. plums;
+delete foods. strawberries;
+// Only change code above this line
+
+console.log(foods);
+
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+
+
+function isEveryoneHere(userObj) {
+  // Only change code below this line
+return["Alan", "Jeff", "Sarah" , "Ryan"].every(name =>
+    userObj.hasOwnProperty(name)
+  );
+  // Only change code above this line
+}
+
+console.log(isEveryoneHere(users));
+
+
+
+const users = {
+  Alan: {
+    online: false
+  },
+  Jeff: {
+    online: true
+  },
+  Sarah: {
+    online: false
+  }
+}
+
+function countOnline(usersObj) {
+  // Only change code below this line
+let result = 0;
+  for (let user in usersObj) {
+    if (usersObj[user].online === true) {
+      result++;
+    }
+  }
+  return result;
+  // Only change code above this line
+}
+
+console.log(countOnline(users));
+
+
+
+let user = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    organization: 'freeCodeCamp',
+    friends: [
+      'Sam',
+      'Kira',
+      'Tomo'
+    ],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+  // Only change code below this line
+  userObj.data.friends.push(friend);
+  return userObj.data.friends;
+  // Only change code above this line
+}
+
+console.log(addFriend(user, 'Pete'));
+
+
+function convertToF(celsius) {
+  let fahrenheit = celsius*(9 / 5) + 32;
+  return fahrenheit;
+}
+
+convertToF(30);
+
+
+
+
+
