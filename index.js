33f2249e15c745605ccf9d50f2c7befa8cb8a198
yuxunan <106828763+yuxunan@users.noreply.github.com>
2022-07-07 11:40:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -586,6 +586,75 @@ Dog.prototype.numLegs = 2;
 let beagle = new Dog("Snoopy");
 
 
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Only change code below this line
+
+for (let property in beagle ){
+  if(beagle.hasOwnProperty(property)){
+    ownProps.push(property);
+  }else{
+    prototypeProps.push(property);
+  }
+}
+
+
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+function joinDogFraternity(candidate) {
+  if (candidate.constructor === Dog) {
+    return true;
+    } else {
+    return false;
+ }
+}
+
+
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Only change code below this line
+ numLegs:4,
+ eat: function(){
+   console.log("nom nom nom");
+ },
+ describe: function(){
+   console.log("My name is " + this.name);
+ }
+};
+
+
+
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+Dog.prototype = {
+ constructor:Dog,
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
 
 
 
