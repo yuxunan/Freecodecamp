16e29f1a604b169a7929ad5728f5f1fdec86de5f
yuxunan <106828763+yuxunan@users.noreply.github.com>
2022-07-07 11:52:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -656,7 +656,55 @@ Dog.prototype = {
   }
 };
 
+function Dog(name) {
+  this.name = name;
+}
 
+let beagle = new Dog("Snoopy");
+
+// Only change code below this line
+
+Dog.prototype.isPrototypeOf(beagle);
+
+
+
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
+
+
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+};
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+   eat: function() {
+    console.log("nom nom nom");
+  }
+};
 
 
 
