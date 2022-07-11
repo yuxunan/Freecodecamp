785ce8f5c248ab5fd3f461d9e4e948cabaf96a8b
yuxunan <106828763+yuxunan@users.noreply.github.com>
2022-07-11 09:34:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -761,3 +761,12 @@ return arr
             </div>
         ));
 
+function fearNotLetter(str) {
+  for (let i = 1; i < str.length; ++i) {
+    if (str.charCodeAt(i) - str.charCodeAt(i - 1) > 1) {
+      return String.fromCharCode(str.charCodeAt(i - 1) + 1);
+    }
+  }
+}
+
+
